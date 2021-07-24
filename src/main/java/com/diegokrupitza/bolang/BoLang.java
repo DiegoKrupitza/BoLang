52d6dffd63d859ba8a8e07b9cbacb0e1d8a62010
Diego Krupitza <info@diegokrupitza.com>
2021-07-24 23:43:40 +0200

    Run BoLang from the commandline with the generated jar

---
+++ src/main/java/com/diegokrupitza/bolang/BoLang.java
@@ -0,0 +1,84 @@
+package com.diegokrupitza.bolang;
+
+import com.diegokrupitza.bolang.syntaxtree.BuildAstVisitor;
+import com.diegokrupitza.bolang.syntaxtree.nodes.BoNode;
+import com.diegokrupitza.bolang.vm.VirtualMachine;
+import com.diegokrupitza.bolang.vm.types.AbstractElementType;
+import com.diegokrupitza.pdfgenerator.BoLexer;
+import com.diegokrupitza.pdfgenerator.BoParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @author Diego Krupitza
+ * @version 1.0
+ * @date 24.07.21
+ */
+public class BoLang {
+
+    public static void main(String[] args) {
+        try {
+            // defining the option for the args processing
+            Options options = new Options();
+
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+
+            List<String> boLangFiles = cmd.getArgList();
+            if (boLangFiles.size() != 1) {
+                //TODO: better exception
+                throw new RuntimeException("You can only run a single BoLang program at once!");
+            }
+
+            String fileName = boLangFiles.get(0);
+            Path boLangCodeFile = Paths.get(fileName);
+
+            String boLangFileContent = Files.readString(boLangCodeFile);
+
+            // lexing
+            BoLexer boLexer = new BoLexer(CharStreams.fromString(boLangFileContent));
+            boLexer.removeErrorListeners();
+            boLexer.addErrorListener(ThrowingErrorListener.INSTANCE);
+
+
+            CommonTokenStream tokens = new CommonTokenStream(boLexer);
+
+            // parsing
+            BoParser boParser = new BoParser(tokens);
+
+            boParser.removeErrorListeners();
+            boParser.addErrorListener(ThrowingErrorListener.INSTANCE);
+
+            BoParser.BoContext bo = boParser.bo();
+
+            // AST generator
+            BuildAstVisitor buildAstVisitor = new BuildAstVisitor();
+            BoNode head = (BoNode) buildAstVisitor.visitBo(bo);
+
+            VirtualMachine virtualMachine = getVirtualMachine(head);
+            AbstractElementType<?> returnVal = virtualMachine.run(null);
+
+            if (returnVal != null) {
+                System.out.println(returnVal.toString());
+            }
+
+        } catch (Exception e) {
+            //TODO better exception handling in the future
+            e.printStackTrace();
+        }
+
+    }
+
+    private static VirtualMachine getVirtualMachine(BoNode head) {
+        return new VirtualMachine(head);
+    }
+}
--- src/main/java/com/diegokrupitza/bolang/ThrowingErrorListener.java
+++ src/main/java/com/diegokrupitza/bolang/ThrowingErrorListener.java
@@ -1,9 +1,23 @@
 package com.diegokrupitza.bolang;
 
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
 /**
  * @author Diego Krupitza
  * @version 1.0
  * @date 24.07.21
+ * https://stackoverflow.com/a/26573239/7835214
  */
-public class ThrowingErrorListener {
+public class ThrowingErrorListener extends BaseErrorListener {
+
+    public static final ThrowingErrorListener INSTANCE = new ThrowingErrorListener();
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+            throws ParseCancellationException {
+        throw new ParseCancellationException("line " + line + ":" + charPositionInLine + " " + msg);
+    }
 }
