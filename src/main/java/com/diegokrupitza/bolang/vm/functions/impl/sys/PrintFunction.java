3c0a464afac001557c998c9c359118ba3bc9d95a
Diego Krupitza <info@diegokrupitza.com>
2021-07-28 10:52:18 +0200

    `print` function now in Sys module
    This allows you to print to the standard output

---
+++ src/main/java/com/diegokrupitza/bolang/vm/functions/impl/sys/PrintFunction.java
@@ -0,0 +1,30 @@
+package com.diegokrupitza.bolang.vm.functions.impl.sys;
+
+import com.diegokrupitza.bolang.vm.functions.BoFunction;
+import com.diegokrupitza.bolang.vm.functions.Function;
+import com.diegokrupitza.bolang.vm.functions.exceptions.BoFunctionException;
+import com.diegokrupitza.bolang.vm.types.AbstractElementType;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * @author Diego Krupitza
+ * @version 1.0
+ * @date 28.07.21
+ */
+@BoFunction(module = "Sys", name = "print")
+@Getter
+public class PrintFunction implements Function {
+    @Override
+    public void paramCheck(List<AbstractElementType<?>> params) throws BoFunctionException {
+        //This is empty since we simply print out all the params
+        return;
+    }
+
+    @Override
+    public AbstractElementType<?> execute(List<AbstractElementType<?>> params) throws BoFunctionException {
+        params.forEach(obj -> System.out.print(obj.getValue()));
+        return null;
+    }
+}
