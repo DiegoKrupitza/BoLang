0e874c3120291c0bbdffcfd027aa7d4ae77773cd
Diego Krupitza <info@diegokrupitza.com>
2021-07-28 13:19:12 +0200

    `Arrays.of()` implemented and Arrays docu improved

--- src/main/java/com/diegokrupitza/bolang/vm/utils/Arrays.java
+++ src/main/java/com/diegokrupitza/bolang/vm/utils/Arrays.java
@@ -5,6 +5,8 @@ import com.diegokrupitza.bolang.vm.types.ArrayElement;
 import com.diegokrupitza.bolang.vm.types.Type;
 import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -14,22 +16,57 @@ import java.util.stream.Collectors;
  */
 public class Arrays {
 
+    private Arrays() {
+        // this class should only be accessed by static methods
+    }
+
+    /**
+     * Generates a new empty array.
+     *
+     * @return an empty array
+     */
     public static ArrayElement emptyArray() {
         return new ArrayElement();
     }
 
-    public static boolean isEmpty(ArrayElement array) {
-        if (!isArray(array)) {
-            throw new IllegalArgumentException("You wanted to call this method with an element not of type array!");
-        }
+    /**
+     * Creates an array with the provided elements
+     *
+     * @param elements the elements we want to be inside the array
+     * @return the newly created array with all the elements in it
+     */
+    public static ArrayElement of(AbstractElementType<?>... elements) {
+        return new ArrayElement(List.of(elements));
+    }
 
+    /**
+     * Checks if an array is empty or not
+     *
+     * @param array the array to check on
+     * @return <code>true</code> when the array is empty otherwise <code>false</code>
+     */
+    public static boolean isEmpty(ArrayElement array) {
         return CollectionUtils.isEmpty(array.getValue());
     }
 
-    public static boolean isArray(AbstractElementType<?> array) {
-        return array.getType() == Type.ARRAY && array instanceof ArrayElement;
+    /**
+     * Checks if the provided element is an array
+     *
+     * @param element the element to check on
+     * @return <code>true</code> when it is an array otherwise <code>false</code>
+     */
+    public static boolean isArray(AbstractElementType<?> element) {
+        return Objects.nonNull(element) && element.getType() == Type.ARRAY && element instanceof ArrayElement;
     }
 
+    /**
+     * Serves the string representation of the array.
+     * An array is display in the following form:
+     * <code>[element1, element2, ...]</code>
+     *
+     * @param array the array of which to get its string representation
+     * @return the string representation of the given array
+     */
     public static String toString(ArrayElement array) {
 
         String valuesAsString = array.getValue().stream()
@@ -41,7 +78,6 @@ public class Arrays {
                     }
                 })
                 .collect(Collectors.joining(", "));
-
 
         return "[" + valuesAsString + "]";
     }
--- src/test/java/com/diegokrupitza/bolang/vm/utils/ArraysUtilTest.java
+++ src/test/java/com/diegokrupitza/bolang/vm/utils/ArraysUtilTest.java
@@ -2,6 +2,8 @@ package com.diegokrupitza.bolang.vm.utils;
 
 import com.diegokrupitza.bolang.vm.types.ArrayElement;
 import com.diegokrupitza.bolang.vm.types.DoubleElement;
+import com.diegokrupitza.bolang.vm.types.IntegerElement;
+import com.diegokrupitza.bolang.vm.types.StringElement;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -13,16 +15,16 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @version 1.0
  * @date 11.07.21
  */
-public class ArraysUtilTest {
+class ArraysUtilTest {
 
     @Test
-    public void generateEmptyArrayTest() {
+    void generateEmptyArrayTest() {
         ArrayElement arrayElement = Arrays.emptyArray();
         assertThat(arrayElement.getValue()).isEmpty();
     }
 
     @Test
-    public void isEmptyArrayTest() {
+    void isEmptyArrayTest() {
         ArrayElement emptyArray = Arrays.emptyArray();
         assertThat(Arrays.isEmpty(emptyArray)).isTrue();
 
@@ -30,4 +32,37 @@ public class ArraysUtilTest {
         assertThat(Arrays.isEmpty(notEmptyArray)).isFalse();
     }
 
+    @Test
+    void createArrayOfTest() {
+        ArrayElement arrayElement = Arrays.of(new StringElement("Test"), new IntegerElement(10));
+        assertThat(arrayElement).isNotNull();
+        assertThat(arrayElement.size()).isEqualTo(2);
+    }
+
+    @Test
+    void isArrayTest() {
+        IntegerElement integerElement = new IntegerElement(10);
+        ArrayElement arrayElement = new ArrayElement();
+        ArrayElement nullArray = null;
+
+        assertThat(Arrays.isArray(integerElement)).isFalse();
+        assertThat(Arrays.isArray(arrayElement)).isTrue();
+        assertThat(Arrays.isArray(nullArray)).isFalse();
+    }
+
+    @Test
+    void toStringArrayTest() {
+        ArrayElement arrayElement = Arrays.of(new IntegerElement(10), new StringElement("Test"));
+        String actual = Arrays.toString(arrayElement);
+
+        assertThat(actual).isEqualToIgnoringWhitespace("[10,\"Test\"]");
+    }
+
+    @Test
+    void toStringNestedArrayTest() {
+        ArrayElement arrayElement = Arrays.of(new IntegerElement(10), Arrays.of(new StringElement("Test"), new IntegerElement(11)));
+        String actual = Arrays.toString(arrayElement);
+
+        assertThat(actual).isEqualToIgnoringWhitespace("[10, [\"Test\", 11]]");
+    }
 }
